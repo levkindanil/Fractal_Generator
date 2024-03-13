de2c2be60fc9761a970de7a03e71957059d091c5
Danil Lyovkin <levkindanil@gmail.com>
2024-03-13 15:01:34 +0300

    Initial commit

---
+++ program/animations.py
@@ -0,0 +1,12 @@
+class AnimationEngine:
+    def __init__(self):
+        # Инициализация модуля создания анимаций фракталов
+        pass
+
+    def create_animation(self, frames, frame_rate):
+        # Создание анимации фрактала на основе последовательности кадров
+        pass
+
+    def play_animation(self):
+        # Воспроизведение анимации
+        pass
