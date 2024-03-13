2b64bd56461b0c339d83a4731fceb4a8bd1bc50b
Danil Lyovkin <levkindanil@gmail.com>
2024-03-13 20:25:53 +0300

    test2

---
+++ program/performance_analyzer.py
@@ -0,0 +1,12 @@
+class PerformanceAnalyzer:
+    def __init__(self):
+        # Инициализация модуля анализа производительности
+        pass
+
+    def analyze_performance(self, algorithm, parameters):
+        # Анализ производительности генерации фрактала с использованием определенного алгоритма
+        pass
+
+    def generate_performance_report(self):
+        # Генерация отчета о производительности
+        pass
